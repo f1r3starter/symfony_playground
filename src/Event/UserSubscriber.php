98919d058df68de84b8fd15eb850d9fdf85a5989
f1r3starter <iamthebest12!A>
2018-12-12 15:58:21 +0200

    user locale

--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -8,7 +8,9 @@
 
 namespace App\Event;
 
+use App\Entity\UserPreferences;
 use App\Mailer\Mailer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
@@ -17,10 +19,24 @@ class UserSubscriber implements EventSubscriberInterface
      * @var Mailer
      */
     private $mailer;
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+    /**
+     * @var string
+     */
+    private $defaultLocale;
 
-    public function __construct(Mailer $mailer)
+    public function __construct(
+        Mailer $mailer,
+        EntityManagerInterface $entityManager,
+        string $defaultLocale
+    )
     {
         $this->mailer = $mailer;
+        $this->entityManager = $entityManager;
+        $this->defaultLocale = $defaultLocale;
     }
 
     public static function getSubscribedEvents()
@@ -32,6 +48,11 @@ class UserSubscriber implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $event)
     {
+        $preferences = new UserPreferences();
+        $preferences->setLocale($this->defaultLocale);
+
+        $this->entityManager->flush();
+
         $this->mailer->sendConfirmationEmail($event->getRegisteredUser());
     }
 }
